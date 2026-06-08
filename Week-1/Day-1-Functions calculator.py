ff1ccdd9f1acf1c99b4d7fc9a571efa920890cc0
Shoaib Shaikh <shoaibshaikh9258@gmail.com>
2026-06-08 10:25:43 +0530

    Update Day-1-Functions calculator.py

--- Week-1/Day-1-Functions calculator.py
+++ Week-1/Day-1-Functions calculator.py
@@ -1,17 +1,24 @@
-#Day 1: Added calculator program
-def add(a,b):
-  return a+b
-def min(a,b):
-  return a-b
-def div(a,b):
-  return a/b
-def multi(a,b):
-  return a*b
-def mod(a,b):
-  return a%b
+# Day 1 - Functions
+# Calculator Program
 
-  print(add(10,5))
-  print(min(10,5))
-  print(div(10,5))
-  print(multi(10,5))
-  print(mod(10,5))
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def div(a, b):
+    return a / b
+
+def multi(a, b):
+    return a * b
+
+def mod(a, b):
+    return a % b
+
+
+print(add(10, 5))
+print(subtract(10, 5))
+print(div(10, 5))
+print(multi(10, 5))
+print(mod(10, 5))
