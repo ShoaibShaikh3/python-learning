3ba81ddd4872fbb36e771289a627e3bc73a3c49b
Shoaib Shaikh <shoaibshaikh9258@gmail.com>
2026-06-08 11:00:19 +0530

    Create  palindrome.py

--- Week-1/Day-1-Functions/palindrome.py
+++ Week-1/Day-1-Functions/palindrome.py
@@ -1,6 +1,12 @@
-n = input("Eneter Number: ")
-if n == n[::-1]:
-  print ("Palindrome")
+# Day 1 - Functions
+# Palindrome Check
+
+def is_palindrome(text):
+    return text == text[::-1]
+
+text = input("Enter a number: ")
+
+if is_palindrome(text):
+    print("Palindrome")
 else:
-  print("Not Palindrome")
-  
+    print("Not Palindrome")
