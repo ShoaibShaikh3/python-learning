0817e25ef644eadf37a3cbf828c3184e47d8aed0
Shoaib Shaikh <shoaibshaikh9258@gmail.com>
2026-06-08 10:22:26 +0530

    Create Day-1-Functions calculator.py

---
+++ Week-1/Day-1-Functions calculator.py
@@ -0,0 +1,17 @@
+#Day 1: Added calculator program
+def add(a,b)
+  return a+b
+def min(a,b)
+  return a-b
+def div(a,b)
+  return a/b
+def multi(a,b)
+  return a*b
+def mod(a,b)
+  return a%b
+
+  print(add(10,5))
+  print(min(10,5))
+  print(div(10,5))
+  print(multi(10,5))
+  print(mod(10,5))
