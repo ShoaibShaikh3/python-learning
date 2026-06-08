da6e9616a1446ce3a189164160a4fcfb814adbb3
Shoaib Shaikh <shoaibshaikh9258@gmail.com>
2026-06-08 10:42:23 +0530

    Create prime.py

---
+++ Week-1/Week-1/Day-1-Functions/prime.py
@@ -0,0 +1,14 @@
+# Day 1 - Functions
+# Prime Number Check
+
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+print(is_prime(7))
